78a1b8bde1f4b30366074219ff9544cbb399b633
pratikhyperfabrics <pratik.patil@hyperfabrics.com>
2019-05-22 16:53:08 -0400

    misc

--- misc/Papernet/launch-orderer.sh
+++ misc/Papernet/launch-orderer.sh
@@ -4,10 +4,10 @@
 # Change the logging leve to the desired level
 
 export  FABRIC_LOGGING_SPEC=info
-export  FABRIC_LOGGING_FORMAT= '%{color}%{time:2006-01-02 15:04:05.000 MST} [%{module}] %{shortfunc} -> %{level:.4s} %{id:03x}%{color:reset} %{message}'
+export  FABRIC_LOGGING_FORMAT= "%{color}%{time:2006-01-02 15:04:05.000 MST} [%{module}] %{shortfunc} -> %{level:.4s} %{id:03x}%{color:reset} %{message}"
 
 export FABRIC_CFG_PATH=$PWD
 
 mkdir -p ledger 
 
-orderer start 2> ./ledger/orderer.log  &
+orderer 2> ./ledger/orderer.log  &
