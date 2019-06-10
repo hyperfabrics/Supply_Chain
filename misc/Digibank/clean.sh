4c0cd644f369100bcac8370c297403a47406b388
pratikhyperfabrics <pratik.patil@hyperfabrics.com>
2019-06-10 16:41:33 -0400

    misc

--- misc/Digibank/clean.sh
+++ misc/Digibank/clean.sh
@@ -2,7 +2,6 @@ rm -rf ledger
 
 killalll peer 2>/dev/null 
 
-
 fuser -n tcp -k 7050
 
 fuser -n tcp -k 7051
@@ -14,6 +13,11 @@ fuser -n tcp -k 7053
 fuser -n tcp -k 5984
 
 rm ./*.log  2> /dev/null
+
+docker kill $(docker ps -q)
+
+docker rm $(docker ps -a -q)
+
 
 echo "done"
 
